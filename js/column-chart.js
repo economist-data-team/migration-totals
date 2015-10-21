24cbf990c6e1d9836dba4a80684db7d9d4c67b98
Evan Hensleigh <futuraprime@gmail.com>
2015-10-21 18:33:51 +0100

    Hey look, it's stacked bars.

--- js/column-chart.js
+++ js/column-chart.js
@@ -1,46 +1,22 @@
 import d3 from 'd3';
 import React from 'react';
 import RFD from 'react-faux-dom';
-import SVGComponent from './svg-component.js';
-import { parseMarginArray } from './utilities.js';
+import BoundedSVG from './bounded-svg.js';
+import { Im } from './utilities.js';
 
-export default class ColumnChart extends SVGComponent {
+class ColumnSeries extends BoundedSVG {
   static get defaultProps() {
-    return {
-      height: 300,
-      width: 595,
-      xAccessor : d => d.x,
-      yAccessor : d => d.y,
-      data : [
-        {y:0.1,x:0},
-        {y:0.3,x:1},
-        {y:0.9,x:2},
-        {y:0.4,x:3}
-      ],
-      margin : 10,
-      spacing : 10,
-      xScale : d3.scale.linear().domain([0,4]),
-      yScale : d3.scale.linear().domain([0,1])
-    };
+    return Im.extend(super.defaultProps, {});
   }
-  get margins() { return parseMarginArray(this.props.margin); }
-  get leftBound() { return this.margin.left; }
-  get rightBound() { return this.props.width - this.margin.right; }
-  get topBound() { return this.margin.top; }
-  get bottomBound() { return this.props.height - this.margin.bottom; }
-  get widthSpan() { return this.props.width - this.margin.left - this.margin.right; }
-  get heightSpan() { return this.props.height - this.margin.top - this.margin.bottom; }
-
   render() {
     var el = RFD.createElement('g');
     var sel = d3.select(el);
 
-    var xScale = this.props.xScale.copy().range([this.leftBound, this.rightBound + this.props.spacing]);
-    var yScale = this.props.yScale.copy().range([this.topBound, this.bottomBound]);
+    var xScale = this.props.xScale, yScale = this.props.yScale;
 
     // spacing dropped here for the last column, which should flush to
     // the end
-    var colWidth = xScale(1) - xScale(0) - this.props.spacing;
+    var colWidth = xScale(1) - xScale(0) - this.props.spacing - this.props.offset;
 
     var columnJoin = sel.selectAll('.chart-column')
       .data(this.props.data);
@@ -48,16 +24,70 @@ export default class ColumnChart extends SVGComponent {
       .append('svg:rect')
       .classed('chart-column', true);
     columnJoin.exit().remove();
-    columnJoin.attr({
-      height : d => yScale(this.props.yAccessor(d)),
-      y : d => this.heightSpan - yScale(this.props.yAccessor(d)),
-      width: colWidth,
-      x : d => xScale(this.props.xAccessor(d))
+    columnJoin
+      .classed(`column-${this.props.index}`, true)
+      .attr({
+        height : d => yScale(this.props.yAccessor(d)) - yScale(0),
+        y : d => {
+          console.log(this.props.index, this.props.yAccessor(d), this.props.priorAccessor(d),
+            yScale(this.props.yAccessor(d) + this.props.priorAccessor(d)));
+          return this.heightSpan - yScale(this.props.yAccessor(d) + this.props.priorAccessor(d));
+        },
+        width: colWidth,
+        x : d => xScale(this.props.xAccessor(d)) + this.props.offset
+      });
+
+    return el.toReact();
+  }
+}
+
+export default class ColumnChart extends BoundedSVG {
+  static get defaultProps() {
+    return {
+      height: 300,
+      width: 595,
+      xAccessor : d => d.x,
+      yAccessor : [d => d.y, d => d.y2, d => d.y3],
+      data : [
+        {x:0, y:10, y2:80, y3:50},
+        {x:1, y:30, y2:50, y3:50},
+        {x:2, y:90, y2:30, y3:50},
+        {x:3, y:40, y2:60, y3:50}
+      ],
+      margin : 10,
+      spacing : 10,
+      xScale : d3.scale.linear().domain([0,4]),
+      yScale : d3.scale.linear().domain([0,300])
+    };
+  }
+  render() {
+    var el = RFD.createElement('g');
+    var sel = d3.select(el);
+
+    var yAccessor = this.props.yAccessor instanceof Array ? this.props.yAccessor : [this.props.yAccessor];
+
+    var xScale = this.props.xScale.copy().range([this.leftBound, this.rightBound + this.props.spacing]);
+    var yScale = this.props.yScale.copy().range([this.topBound, this.bottomBound]);
+
+    var columns = yAccessor.map((accessor, idx) => {
+      var props = {
+        key : accessor,
+        xScale : xScale, yScale : yScale,
+        data : this.props.data,
+        xAccessor : this.props.xAccessor,
+        yAccessor : accessor,
+        priorAccessor : d => yAccessor.slice(0,idx).reduce((memo, fn) => { return memo + fn(d); }, 0),
+        spacing : this.props.spacing,
+        margin : this.props.margin,
+        index : idx,
+        offset : 0 // placeholder: to handle adjacent series
+      };
+      return (<ColumnSeries {...props} />);
     });
 
     return(
       <svg height={this.props.height} width={this.props.width}>
-        {el.toReact()}
+        {columns}
       </svg>
     );
   }
